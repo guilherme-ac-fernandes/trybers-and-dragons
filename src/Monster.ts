91ad4b5b4562ed014d346a9a3de685000a78b3e2
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-22 20:11:57 -0300

    feat: criação da classe Monster

---
+++ src/Monster.ts
@@ -0,0 +1,30 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  constructor(
+    private _lifePoints = 85,
+    private _strength = 63,
+  ) {
+    this._lifePoints = _lifePoints;
+    this._strength = _strength;
+  }
+
+  public get lifePoints(): number { return this._lifePoints; }
+  public get strength(): number { return this._strength; }
+
+  receiveDamage(attackPoints: number): number {
+    const lifeDamage = this._lifePoints - attackPoints;
+
+    if (lifeDamage > 0) {
+      this._lifePoints = lifeDamage;
+      if (lifeDamage <= 0) {
+        this._lifePoints = -1;
+      }
+    }
+    return this._lifePoints;
+  }
+  
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
