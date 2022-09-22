d6365adbbf871b2c4130c154e6c6b3726f3a4146
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-22 15:03:18 -0300

    feat: criação da classe abstrata Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,21 @@
+export default abstract class Race {
+  constructor(
+    private _name: string,
+    private _dexterity: number,
+  ) {
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
