3ffc0838fe027397a5a7fa1b5f030ebaaec5f376
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-22 23:08:33 -0300

    feat: inicia criação do index com personagens e alteração no pve

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -1,5 +1,5 @@
 import Fighter, { SimpleFighter } from '../Fighter';
-import getRandomInt from '../utils';
+// import getRandomInt from '../utils';
 import Battle from './Battle';
 
 export default class PVE extends Battle {
@@ -10,33 +10,46 @@ export default class PVE extends Battle {
     super(_fighter);
   }
 
-  public get fighter(): Fighter {
-    return this._fighter;
-  }
-
-  public get enemies(): SimpleFighter[] {
-    return this._enemies;
-  }
+  public get fighter(): Fighter { return this._fighter; }
+  public get enemies(): SimpleFighter[] { return this._enemies; }
   
   fight(): number {
-    const enemyLifePoints = this._enemies
-      .reduce((acc, curr) => acc + curr.lifePoints, 0);
+    // const enemyLifePoints = this._enemies
+    //   .reduce((acc, curr) => acc + curr.lifePoints, 0);
 
-    while (this._fighter.lifePoints > 0 && enemyLifePoints > 0) {
-      const index = getRandomInt(0, this._enemies.length);
-      const fighterStrength = this._fighter.strength;
-      const enemyStrength = this._enemies[index].strength;
+    // while (this._fighter.lifePoints > 0 && enemyLifePoints > 0) {
+    //   const index = getRandomInt(0, this._enemies.length);
+    //   const fighterStrength = getRandomInt(1, this._fighter.strength);
+    //   const enemyStrength = getRandomInt(1, this._enemies[index].strength);
       
-      // Ataque de turno do hero
-      this._fighter.attack(this._enemies[index]);
-      this._enemies[index].receiveDamage(getRandomInt(1, fighterStrength));
+    //   // Ataque de turno do hero
+    //   this._fighter.attack(this._enemies[index]);
+    //   this._enemies[index].receiveDamage(fighterStrength);
       
-      // Ataque de um inimigo
-      this._enemies[index].attack(this._fighter);
-      this._fighter.receiveDamage(getRandomInt(1, enemyStrength));
-    }
+    //   // Ataque de um inimigo
+    //   this._enemies[index].attack(this._fighter);
+    //   this._fighter.receiveDamage(enemyStrength);
+    // }
 
     const result = super.fight();
     return result;
   }
 }
+
+// const fight = (battle: Battle) => battle.fight();
+
+// const result = () => {
+//   const player1 = new Character('');
+//   for (let i = 0; i < 500; i += 1) player1.levelUp();
+//   const monster = new Monster();
+//   const pve1 = new PVE(player1, [monster]);
+
+//   const player2 = new Character('');
+//   const monsters: Monster[] = [];
+//   for (let i = 0; i < 500; i += 1) monsters.push(new Monster());
+//   const pve2 = new PVE(player2, monsters);
+
+//   return [fight(pve1), fight(pve2)];
+// };
+
+// console.log(result());
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+// Lutadores
+export const player1 = new Character('player1');
+export const player2 = new Character('player2');
+export const player3 = new Character('player3');
+for (let i = 0; i < 50; i += 1) player1.levelUp();  
+
+// Monstros
+export const monster1 = new Monster();
+export const monster2 = new Dragon();
+
+// Batalhas
+export const pvp = new PVP(player2, player3);
+export const pve = new PVE(player1, [monster1, monster2]);
+
+export const runBattles = (battles: Battle[]): void => {
+  console.log(battles);
+  
+  // for (let i = 0; i < 50; i += 1) {
+  //   battles[i].fight();
+  // }  
+};
