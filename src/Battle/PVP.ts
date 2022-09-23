48f8c6bef71836cf62c2b5cf054f4c7ef0ac32f1
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-22 21:14:51 -0300

    refactor: remove testes de batalha pvp

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -30,21 +30,3 @@ export default class PVP extends Battle {
     return result;
   }
 }
-
-const fight = (battle: Battle) => battle.fight();
-
-const result = () => {
-  const player1 = new Character('');
-  for (let i = 0; i < 500; i += 1) player1.levelUp();
-  const player2 = new Character('');
-  const pvp1 = new PVP(player1, player2);
-
-  const player3 = new Character('');
-  for (let i = 0; i < 500; i += 1) player3.levelUp();
-  const player4 = new Character('');
-  const pvp2 = new PVP(player4, player3);
-
-  return [fight(pvp1), fight(pvp2)];
-};
-
-console.log(result());
